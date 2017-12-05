d0adb4f8fcd110ba185885b618d58a1096ecdeac
Robert Baart <robert.baart@gmail.com>
2017-12-05 16:33:27 +0100

    Added function to update the bills to put in the grey safe automatically
    The function is not called from anywhere yet as I couldn't quickly
    figure out how to do so, so for now it doesn't actually update
    
    Signed-off-by: Robert Baart <robert.baart@gmail.com>

--- src/BarSession.js
+++ src/BarSession.js
@@ -4,7 +4,7 @@ export default class BarSession {
   constructor () {
     this.initialCashState = new CashState()
     this.finalCashState = new CashState()
-    this.effluentCashState = new CashState()
+    this.effluentCashState = new CashState(false)
 
     this.theoreticalCashTotal = 0
     this.theoreticalPinTotal = 0
@@ -26,5 +26,25 @@ export default class BarSession {
 
   changeSafeTotal () {
     return this.revenueTotal() - this.effluentTotal()
+  }
+
+  totalToRemove () {
+    // Reset money to put in the grey safe
+    this.effluentCashState.bills.map(bill => { bill.count = 0 })
+
+    // Always put all 50 and 100 euro bills in the grey safe
+    this.effluentCashState.bills[0].count = this.finalCashState.bills[0].count
+    this.effluentCashState.bills[1].count = this.finalCashState.bills[1].count
+
+    // Starting with the 20 euro bills, put more in the grey safe until the remainder
+    // is close to 200
+    var nextBill = 2
+    while (this.finalCashState.total() - this.effluentCashState.total() > 220) {
+      if (this.finalCashState.bills[nextBill].count > this.effluentCashState.bills[nextBill].count) {
+        this.effluentCashState.bills[nextBill].count++
+      } else {
+        nextBill++
+      }
+    }
   }
 }
