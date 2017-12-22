f822fcf65380af26c977ee579d3cdb1f2f0c5fb0
Casper Boone <mail@casperboone.nl>
2017-12-22 20:45:20 +0100

    Change var to let

--- src/BarSession.js
+++ src/BarSession.js
@@ -28,7 +28,7 @@ export default class BarSession {
   }
 
   effluentCashState () {
-    var state = new CashState(false)
+    let state = new CashState(false)
 
     // Always put all 50 and 100 euro bills in the grey safe
     state.bills[0].count = this.finalCashState.bills[0].count
@@ -36,7 +36,7 @@ export default class BarSession {
 
     // Starting with the 20 euro bills, put more in the grey safe until the remainder
     // is close to 200
-    var nextBill = 2
+    let nextBill = 2
     while (this.finalCashState.total() - state.total() > 220) {
       state.bills[nextBill].count = Math.min(
         this.finalCashState.bills[nextBill].count,
