a0f22d8f79b81333b821ffc6c87ac4480642d180
Casper Boone <mail@casperboone.nl>
2018-05-29 00:31:12 +0200

    Add cash specification ticket

---
+++ src/renderer/components/BarSessionCashSpecificationTicket.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <img src="../assets/images/logo.png" class="h-12 " />
+
+        <div style="margin: 35px 0;">
+            <strong>Inhoudsspecificatie Kassalade</strong><br />
+            Laatste gebruik:<br />
+            {{ barSession.type.name }}<br />
+            {{ moment(barSession.date).format('DD-MM-YYYY') }}
+        </div>
+
+       
+            <table style="width: 100%;" border="1">
+                <tr v-for="(bill, billKey) in grossBills" :key="bill.amount">
+                    <td>
+                        {{ bill.count - subtractBills[billKey].count }}
+                    </td>
+                    <td>
+                         x &euro; {{ bill.amount | currency('') }}
+                    </td>
+                    <td style="text-align: right;">
+                        &euro; {{ bill.total() - subtractBills[billKey].total() | currency('') }}
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">Noodwisselgeld</td>
+                    <td style="text-align: right;">&euro; {{ grossState.emergencyCash - subtractState.emergencyCash | currency('') }}</td>
+                </tr>
+                <tr>
+                    <td colspan="2">Totaal</td>
+                    <td style="text-align: right; border-top: 1px solid;">&euro; {{ grossState.total() - subtractState.total() | currency('') }}</td>
+                </tr>
+            </table>
+
+    </div>
+</template>
+
+<script>
+import Totals from '@/components/Totals.vue'
+
+export default {
+  props: ['barSession', 'cashStates'],
+  data () {
+    return {
+      moment: window.moment,
+      grossState: this.cashStates[1].cashState(),
+      subtractState: this.cashStates[2].cashState(),
+      grossBills: this.cashStates[1].cashState().bills,
+      subtractBills: this.cashStates[2].cashState().bills
+    }
+  },
+  components: {
+    'totals': Totals
+  }
+}
+</script>
--- src/renderer/components/BarSessionPrinter.vue
+++ src/renderer/components/BarSessionPrinter.vue
@@ -2,6 +2,7 @@
     <div class="inline">
         <button @click="print" class="bg-white border border-teal-dark hover:bg-teal-dark hover:text-white text-teal-dark font-bold py-2 px-4 ml-2 rounded">Afdrukken</button>
         <bar-session-ticket v-show="false" ref="ticket" :bar-session="barSession" :cash-states="cashStates"></bar-session-ticket>
+        <bar-session-cash-specification-ticket v-show="false" ref="ticketCashSpecification" :bar-session="barSession" :cash-states="cashStates"></bar-session-cash-specification-ticket>
     </div>
 </template>
 
@@ -9,15 +10,18 @@
 import { ipcRenderer } from 'electron'
 
 import BarSessionTicket from '@/components/BarSessionTicket'
+import BarSessionCashSpecificationTicket from '@/components/BarSessionCashSpecificationTicket'
 
 export default {
   props: ['barSession', 'cashStates'],
   components: {
-    'bar-session-ticket': BarSessionTicket
+    'bar-session-ticket': BarSessionTicket,
+    'bar-session-cash-specification-ticket': BarSessionCashSpecificationTicket
   },
   methods: {
     print () {
       ipcRenderer.send('print', this.$refs.ticket.$el.innerHTML)
+      ipcRenderer.send('print', this.$refs.ticketCashSpecification.$el.innerHTML)
     }
   }
 }
